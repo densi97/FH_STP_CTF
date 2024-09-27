f47635575a38ad15d1406477669b34340fcf5d17
Fabian <densi97@t-online.de>
2024-09-27 09:29:51 +0200

    Fixed protocol

--- ctf.tf
+++ ctf.tf
@@ -87,14 +87,14 @@ resource "aws_security_group" "FHSTP_CTF_sg" {
   ingress {
     from_port   = 0
     to_port     = 80
-    protocol    = "http"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 0
     to_port     = 443
-    protocol    = "https"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
